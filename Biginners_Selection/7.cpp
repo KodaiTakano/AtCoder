4fb6459a68e626374d2f753e69a25ba6f8db34b6
KodaiTakano <takano_x_kodai@icloud.com>
2022-04-06 21:50:36 +0900

    Bignners_Selection

--- Biginners_Selection/7.cpp
+++ Biginners_Selection/7.cpp
@@ -1,39 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
 
 int main (void){
+    // ifstream in("./../input.txt");
+    // cin.rdbuf(in.rdbuf());
+
     int N;
     cin >> N;
-    vector<int> a(N);
-    for(int i = 0; i < N; i++){
-        cin >> a.at(i);
-    }
+    vector<int> d(N);
+    rep(i, N) cin >> d.at(i);
 
-    int max1, max2, answer = 0;
-    for(int i = 0; i < N; i++){
-        max1 = *max_element(a.begin(), a.end());
-        // cout << max1 << endl;
-        // 最大値だったところを0に
-        for(int j = 0; j < N; j++){
-            if(a.at(j) == max1){
-                a.at(j) = 0;
+    vector<int> a;
+    // rep(i, N) cout << a.at(i) << endl;
+    a.push_back(d.at(0));
+
+    rep(i, N){
+        rep(j, a.size()){
+            if(d.at(i) == a.at(j)){
                 break;
+            }else if(j == a.size()-1){
+                a.push_back(d.at(i));
             }
         }
-        // 2番目の最大値取得
-        max2 = *max_element(a.begin(), a.end());
-        // cout << max1 << endl;
-        // 最大値だったところを0に
-        for(int j = 0; j < N; j++){
-            if(a.at(j) == max2){
-                a.at(j) = 0;
-                break;
-            }
-        }
-        answer += max1 - max2;
     }
 
-    cout << answer << endl;
+    cout << a.size() << endl;
 
     return 0;
 }
---
+++ Biginners_Selection/8.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main (void){
+    ifstream in("./../input.txt");
+    cin.rdbuf(in.rdbuf());
+
+    int N, Y;
+    cin >> N >> Y;
+
+    // dp[n][y]: n枚でy円となることがありうるなら1,あり得ないなら0    
+    vector<vector<int>> dp(N+1, vector<int>(Y+1));
+    //　初期値
+    dp.at(0).at(0) = 1;
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < Y+1; j+=1000){
+            // cout << j << endl;
+            if(dp.at(i).at(j) == 1){
+                if(j + 1000 <= Y) dp.at(i+1).at(j+1000) = 1;
+                if(j + 5000 <= Y) dp.at(i+1).at(j+5000) = 1;
+                if(j + 10000 <= Y) dp.at(i+1).at(j+10000) = 1;
+            }
+        }
+    }
+
+    cout << "check" << endl;
+
+    if(dp.at(N).at(Y) == 1){
+
+    }else{
+        cout << "-1 -1 -1" << endl;
+    }
+
+    return 0;
+}
