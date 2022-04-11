ba8f91e99a5373ba46a0710d582a56add51e05c6
KodaiTakano <takano_x_kodai@icloud.com>
2022-04-12 08:28:54 +0900

    コードを綺麗にした

--- Biginners_Selection/10.cpp
+++ Biginners_Selection/10.cpp
@@ -3,29 +3,10 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
 // 時刻tに(x,y)に存在することが可能なら1
-bool check(int t_i, int t_i_1, int x_i, int x_i_1, int y_i, int y_i_1, int i){
-    // t(i-1)の座標からt(i)の座標に移動できなければreturn 0;
-    if(i > 0){
-        if((t_i-t_i_1) < (abs(x_i-x_i_1)+abs(y_i-y_i_1))){
-            return 0;
-        }
-    }
-    if((x_i+y_i) <= t_i){
-        switch ((x_i+y_i)%2){
-            case 0:
-                if(t_i%2 == 0) return 1;
-                break;
-            case 1:
-                if(t_i%2 == 1) return 1;
-                break;
-            default:
-                return 0;
-        }
-    }
-    return 0;
-}
-
-bool check_0(int t_i, int x_i, int y_i){
+bool check(int t_i, int x_i, int y_i, int t_i_1=0, int x_i_1=0, int y_i_1=0, int i=0){
+    // t(i-1)の座標からt(i)の座標に移動できるか確認
+    if(i > 0 && ((t_i-t_i_1) < (abs(x_i-x_i_1)+abs(y_i-y_i_1)))) return 0;
+    
     if((x_i+y_i) <= t_i){
         switch ((x_i+y_i)%2){
             case 0:
@@ -52,12 +33,13 @@ int main (void){
 
     rep(i,N){
         if(i == 0){
-            if(!check_0(t.at(i), x.at(i), y.at(i))){
+            if(!check(t.at(i), x.at(i), y.at(i))){
                 cout << "No" << endl;
                 return 0;
             }
         }else{
-            if(!check(t.at(i), t.at(i-1), x.at(i), x.at(i-1), y.at(i), y.at(i-1), i)){
+            // 本当はt, x, yそのものを渡したかったけど、計算時間オーバーになった
+            if(!check(t.at(i), x.at(i), y.at(i), t.at(i-1), x.at(i-1), y.at(i-1), i)){
                 cout << "No" << endl;
                 return 0;
             }
