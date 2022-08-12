8119f406f1b96c69b163300e3ee54b4e4ef91609
KodaiTakano <takano_x_kodai@icloud.com>
2022-08-12 20:56:31 +0900

    ABC211 A

---
+++ ABC211/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep1(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define ll long long 
+#define all(x) x.begin(), x.end()
+
+int main (void){
+    // ifstream in("./../input.txt");
+    // cin.rdbuf(in.rdbuf());
+
+    ll A, B;
+    cin >> A >> B;
+    cout << (double)(A-B)/3 + B << endl;
+
+    return 0;
+}
