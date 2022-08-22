4b1f2fc063b632420a0117365c6cc1618e8cbd01
KodaiTakano <takano_x_kodai@icloud.com>
2022-08-22 21:33:03 +0900

    ABC265 D

--- ABC265/D.cpp
+++ ABC265/D.cpp
@@ -6,19 +6,27 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 
 int main (void){
-    ifstream in("./../input.txt");
-    cin.rdbuf(in.rdbuf());
+    // ifstream in("./../input.txt");
+    // cin.rdbuf(in.rdbuf());
 
     ll N, P, Q, R;
     cin >> N >> P >> Q >> R;
-    vector<ll> A(N);
-    rep(i, N) cin >> A.at(i);
-
-    
-    rep(i, N){
-
+    vector<ll> S(N+1, 0);
+    rep1(i, 1, N+1) {
+        ll A;
+        cin >> A;
+        S.at(i)=S.at(i-1)+A;
     }
 
+    rep(x, N){
+        // S.at(y)=P+S.at(x) S.at(z)=Q+S.at(y)=Q+P+S.at(x)
+        if(binary_search(all(S), P+S.at(x)) && binary_search(all(S), P+Q+S.at(x)) && binary_search(all(S), P+Q+R+S.at(x))){
+            // cout << x << endl;
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }   
+    cout << "No" << endl;
 
     return 0;
 }
