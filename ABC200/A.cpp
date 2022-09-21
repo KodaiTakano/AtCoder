179f723a057e1f70a376f1276b05926da47f4fd3
KodaiTakano <takano_x_kodai@icloud.com>
2022-09-21 17:16:43 +0900

    ABC200 A

---
+++ ABC200/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep1(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define ll long long 
+#define all(x) x.begin(), x.end()
+
+int main (void){
+    // ifstream in("./../input.txt");
+    // cin.rdbuf(in.rdbuf());
+
+    ll N;
+    cin >> N;
+
+    if(N%100==0){
+        cout << N/100 << endl;
+    }else{
+        cout << N/100+1 << endl;
+    }
+
+
+    return 0;
+}
